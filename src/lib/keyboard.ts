7bd5d59f8c0df868c45802c1f02ff24e15d38936
Juakiz <juakiz@gmail.com>
2020-06-24 02:08:25 +0100

    Fix keyboard

--- src/lib/keyboard.ts
+++ src/lib/keyboard.ts
@@ -3,6 +3,7 @@ interface Key {
   isDown: boolean;
   isUp: boolean;
   press?(): void;
+  release?(): void;
 }
 
 export default function keyboard(value: string): Key {
@@ -11,16 +12,40 @@ export default function keyboard(value: string): Key {
     isDown: false,
     isUp: true,
   };
-  window.addEventListener(
-    "keydown", ((event: KeyboardEvent): void => {
-      if (event.key === key.value) {
-        if (key.isUp && key.press) key.press();
-        key.isDown = true;
-        key.isUp = false;
-        event.preventDefault();
-      }
-    }).bind(key), false
-  );
+  // The `downHandler`
+  const downHandler = (event: KeyboardEvent): void => {
+    if (event.key === key.value) {
+      if (key.isUp && key.press) key.press();
+      key.isDown = true;
+      key.isUp = false;
+      event.preventDefault();
+    }
+  };
+  //The `upHandler`
+  const upHandler = (event: KeyboardEvent): void => {
+    if (event.key === key.value) {
+      if (key.isDown && key.release) key.release();
+      key.isDown = false;
+      key.isUp = true;
+      event.preventDefault();
+    }
+  };
+
+  //Attach event listeners
+  if (downHandler && upHandler) {
+    window.addEventListener(
+      "keydown", downHandler.bind(key), false
+    );
+    window.addEventListener(
+      "keyup", upHandler.bind(key), false
+    );
+  }
+
+  // // Detach event listeners
+  // key.unsubscribe = () => {
+  //   window.removeEventListener("keydown", downListener);
+  //   window.removeEventListener("keyup", upListener);
+  // };
 
   return key;
 }
